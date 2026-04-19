30dd888d8a20678184a3e20eabd018bbf131096a
Mohit Computers <Mohit Computers@DESKTOP-ETV4NT9>
2026-04-19 20:55:40 +0500

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('LMS_University_Project_FrontEnd_2026');
+}
